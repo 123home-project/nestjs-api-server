32ed20bd7372788eb22a5c5c09dcbd69a2693918
JoDoul <ryan1357ryan@gmail.com>
2024-07-10 15:53:54 +0900

    feat: 로그인 request dto 추가

--- src/user/dto/login.dto.ts
+++ src/user/dto/login.dto.ts
@@ -1,7 +1,23 @@
-import { IsEnum } from 'class-validator';
+import { IsEnum, IsString, ValidateNested } from 'class-validator';
 import { SignUpPlatformType } from '../type/SignUpPlatformType';
+import { Type } from 'class-transformer';
 
+export class NormalLoginDto {
+  @IsString()
+  id: string;
+
+  @IsString()
+  pw: string;
+}
 export class LoginDto {
   @IsEnum(SignUpPlatformType)
   platform: SignUpPlatformType;
+
+  @ValidateNested()
+  @Type((dto) => {
+    if (dto.object.platform === 'Normal') {
+      return NormalLoginDto;
+    }
+  })
+  data: NormalLoginDto;
 }
