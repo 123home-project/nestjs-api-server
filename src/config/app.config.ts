a4febfdfbaa0efe277756da48752ad9c81e29ad3
JoDoul <ryan1357ryan@gmail.com>
2024-11-22 10:43:41 +0900

    fix: 누락된 config 옵션 추가

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,8 +1,10 @@
 import { ConfigModuleOptions } from '@nestjs/config';
 import googleConfig from './google.config';
+import kakaoConfig from './kakao.config';
+import naverConfig from './naver.config';
 
 export default (): ConfigModuleOptions => ({
   envFilePath: `src/config/env/.${process.env.NODE_ENV}.env`,
-  load: [googleConfig],
+  load: [googleConfig, kakaoConfig, naverConfig],
   isGlobal: true,
 });
